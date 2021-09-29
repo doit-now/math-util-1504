3cdcca44400210ba0351c05aaf4b3356e1d6f66c
doit-now <hoang.nguyenthe@gmail.com>
2021-09-29 10:08:22 +0700

    modify code to see RED ALERT

--- test/com/giaolang/mathutil/core/AdvancedFactorialTest.java
+++ test/com/giaolang/mathutil/core/AdvancedFactorialTest.java
@@ -34,7 +34,8 @@ public class AdvancedFactorialTest {
                                 {3, 6},
                                 {4, 24},
                                 {5, 120},
-                                {6, 720}                
+                                {6, 7720}, 
+                                {0, 1}
                                };        
     } 
     
