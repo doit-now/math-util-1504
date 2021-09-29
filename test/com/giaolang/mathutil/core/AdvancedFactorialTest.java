f007401ca7374fdb33bb1936e35988fba5a92310
doit-now <hoang.nguyenthe@gmail.com>
2021-09-29 10:15:46 +0700

    update code to see GREEN 4EVER

--- test/com/giaolang/mathutil/core/AdvancedFactorialTest.java
+++ test/com/giaolang/mathutil/core/AdvancedFactorialTest.java
@@ -34,7 +34,7 @@ public class AdvancedFactorialTest {
                                 {3, 6},
                                 {4, 24},
                                 {5, 120},
-                                {6, 7720}, 
+                                {6, 720}, 
                                 {0, 1}
                                };        
     } 
