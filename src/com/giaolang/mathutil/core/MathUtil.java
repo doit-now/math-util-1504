80624c8b4ec373d2644323f7960fa2597a1f2237
doit-now <hoang.nguyenthe@gmail.com>
2021-10-01 10:27:20 +0700

    add recursion to see GREEN 4EVER again - regression testing

--- src/com/giaolang/mathutil/core/MathUtil.java
+++ src/com/giaolang/mathutil/core/MathUtil.java
@@ -18,19 +18,39 @@ public class MathUtil {
     //TDD - Test Driven Development - viết test trước khi viết code chính
     //Test First Development, viết, suy nghĩ về bộ test case/các tình huống
     //chạy hàm trước khi viết hàm
+    
+    //Đệ quy, recursion, gọi lại chính mình với quy mô khác
+    //Búp bê người Nga
+    //5! = 1.2.3.4.5
+    //5! = 5 * 4!
+    //4! = 4 * 3!
+    //3! = 3 * 2!
+    //2! = 2 * 1!
+    //1! = 1; //dừng
+    //n! = n * (n - 1)! công thức lặp lại chính mình
     public static long getFactorial(int n) {
-        
-        if (n < 0 || n >= 21)
+        if (n < 0 || n > 20)
             throw new IllegalArgumentException("n must be between 0..20");
         
         if (n == 0 || n == 1)
-            return 1;   //n đặc biệt dừng luôn 
+            return 1; //điểm dừng đệ quy
         
-        long product = 1;  //khởi đầu giai thừa là 1
-        
-        for (int i = 2; i <= n; i++) 
-            product *= i;
-        
-        return product;
-    }    
+        return n * getFactorial(n - 1); //xong
+    }
+    
+//    public static long getFactorial(int n) {
+//        
+//        if (n < 0 || n >= 21)
+//            throw new IllegalArgumentException("n must be between 0..20");
+//        
+//        if (n == 0 || n == 1)
+//            return 1;   //n đặc biệt dừng luôn 
+//        
+//        long product = 1;  //khởi đầu giai thừa là 1
+//        
+//        for (int i = 2; i <= n; i++) 
+//            product *= i;
+//        
+//        return product;
+//    }    
 }
